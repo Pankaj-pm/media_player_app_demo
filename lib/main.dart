3c4f800e2a14eaa4d987f9ef993f46e04e3b0fd8
AnandDL02 <ananddalsaniya2002@gmail.com>
2024-05-27 09:38:53 +0530

    audio player

--- lib/main.dart
+++ lib/main.dart
@@ -37,12 +37,38 @@ class _MyHomePageState extends State<MyHomePage> {
     assetsAudioPlayer.open(
         Playlist(
           audios: [
-            Audio("assets/audio/maan_meri_jaan.mp3"),
-            Audio("assets/audio/naach.mp3"),
+            Audio(
+              "assets/audio/maan_meri_jaan.mp3",
+              metas: Metas(
+                title: "Maan meri jaan",
+                image: const MetasImage.network(
+                    "https://www.pagalworld.online/GpE34Kg9Gq/113834/152477-pushpa-pushpa-from-pushpa-2-the-rule-hindi-mp3-song-300.jpg"),
+              ),
+            ),
+            Audio(
+              "assets/audio/naach.mp3",
+              metas: Metas(
+                title: "Naach meri jaan",
+                image: const MetasImage.network(
+                    "https://www.pagalworld.online/GpE34Kg9Gq/113834/152477-pushpa-pushpa-from-pushpa-2-the-rule-hindi-mp3-song-300.jpg"),
+              ),
+            ),
             Audio.network(
-                "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Music_for_Video/springtide/Sounds_strange_weird_but_unmistakably_romantic_Vol1/springtide_-_03_-_We_Are_Heading_to_the_East.mp3"),
-            Audio("assets/audio/fateh_1.mp3"),
-
+              "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Music_for_Video/springtide/Sounds_strange_weird_but_unmistakably_romantic_Vol1/springtide_-_03_-_We_Are_Heading_to_the_East.mp3",
+              metas: Metas(
+                title: "Sringtide",
+                image: const MetasImage.network(
+                    "https://www.pagalworld.online/GpE34Kg9Gq/113834/152477-pushpa-pushpa-from-pushpa-2-the-rule-hindi-mp3-song-300.jpg"),
+              ),
+            ),
+            Audio(
+              "assets/audio/fateh_1.mp3",
+              metas: Metas(
+                title: "Fateh",
+                image: const MetasImage.network(
+                    "https://www.pagalworld.online/GpE34Kg9Gq/113604/152329-daaku-mp3-song-300.jpg"),
+              ),
+            ),
           ],
         ),
         autoStart: false);
@@ -61,18 +87,46 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CarouselSlider(
-              items: List.generate(5, (index) {
+              items: List.generate(assetsAudioPlayer.playlist?.audios.length ?? 0, (index) {
+                var audio = assetsAudioPlayer.playlist?.audios[index];
                 return Container(
                   color: Colors.black12,
                   width: double.infinity,
                   margin: EdgeInsets.all(1),
-                  child: Text("index $index"),
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Image.network(audio?.metas.image?.path ?? "", fit: BoxFit.fill),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          height: 80,
+                          padding: EdgeInsets.all(15),
+                          decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.white,
+                                Colors.transparent,
+                              ],
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                            ),
+                          ),
+                          child: Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(audio?.metas.title ?? "",style: TextStyle(fontWeight: FontWeight.bold)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               }),
               options: CarouselOptions(
                 aspectRatio: 2,
-                autoPlay: true,
-                reverse: true,
+                autoPlay: false,
                 enlargeCenterPage: true,
                 enlargeFactor: 0.2,
                 viewportFraction: 0.9,
@@ -80,6 +134,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 onPageChanged: (index, reason) {
                   cIndex = index;
                   setState(() {});
+                  var audio=assetsAudioPlayer.playlist?.audios[index];
+                  if(audio!=null){
+                    assetsAudioPlayer.open(audio);
+                  }
+
                   print("index $index");
                 },
               ),
@@ -102,9 +161,46 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
               ),
             ),
-            Slider(value: 0.5, onChanged: (value) {
+            StreamBuilder<Duration>(
+                stream: assetsAudioPlayer.currentPosition,
+                builder: (context, snapshot) {
+                  print("currentPosition ${snapshot.data?.inSeconds}");
+                  print("currentPosition1 ${assetsAudioPlayer.current.hasValue}");
 
-            },),
+                  Duration? currentDuration;
+                  if(assetsAudioPlayer.current.hasValue){
+                    currentDuration = assetsAudioPlayer.current.value?.audio.duration;
+                  }
+
+                  return Opacity(
+                    opacity: assetsAudioPlayer.current.hasValue ?1 :0.2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text("${snapshot.data?.inMinutes ?? 0.0}:"),
+                              Text("${(snapshot.data?.inSeconds ?? 0.0) % 60}".padLeft(2, '0')),
+                              Expanded(
+                                child: Slider(
+                                  value: snapshot.data?.inSeconds.toDouble()??0,
+                                  min: 0,
+                                  max: currentDuration?.inSeconds.toDouble()??1,
+                                  onChanged: (value) {
+                                    print("onChanged $value");
+                                    assetsAudioPlayer.seek(Duration(seconds: value.toInt()));
+                                  },
+                                ),
+                              ),
+                              Text("${currentDuration?.inMinutes ?? 0.0}:${currentDuration?.inSeconds ?? 0.0}"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
